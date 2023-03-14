d16ca57603a0c799987e377dab2852e6e348a90b
Artem <artem.sivokoz@gmail.com>
2023-03-14 21:01:36 +0300

    Add models for data

---
+++ app/src/main/java/com/example/programm_for_test/models/MarketCapPercentage.kt
@@ -0,0 +1,14 @@
+package com.example.programm_for_test.models
+
+data class MarketCapPercentage(
+    val ada: Double,
+    val bnb: Double,
+    val btc: Double,
+    val doge: Double,
+    val eth: Double,
+    val matic: Double,
+    val steth: Double,
+    val usdc: Double,
+    val usdt: Double,
+    val xrp: Double
+)
